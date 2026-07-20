762c0e5bec254dece8ca9d3933a653af08053845
Steve Temple <steve@gibedigital.com>
2026-07-20 15:18:01 +0100

    Remove test noficiation in NotifyEditor

--- Umbraco.Automate/src/Umbraco.Automate.Core/Actions/BuiltIn/NotifyEditorAction.cs
+++ Umbraco.Automate/src/Umbraco.Automate.Core/Actions/BuiltIn/NotifyEditorAction.cs
@@ -92,8 +92,6 @@ public sealed class NotifyEditorAction : ActionBase<NotifyEditorSettings, Notify
 
         await _editorNotifier.NotifyAsync(message, cancellationToken);
 
-        context.LogInfo($"Notified editor of content {contentKey} — \"{title}\": {body}");
-
         return Success(new NotifyEditorOutput
         {
             ContentKey = contentKey
